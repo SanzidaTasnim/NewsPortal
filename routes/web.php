d370af1551534b603d8bf794f1504694b32b201b
tasnim5sanzida@gmail.com <tasnim5sanzida@gmail.com>
2023-07-02 01:09:50 +0600

    backend & frontend template set

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.home');
+});
+Route::get('/category', function () {
+    return view('layouts.app');
 });
